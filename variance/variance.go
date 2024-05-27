2fb50298fd025be8f43c1ea82c55316b543bb877
wyonyango <wycliffealphus7@gmail.com>
2024-05-27 12:33:46 +0300

    style: add comments

--- variance/variance.go
+++ variance/variance.go
@@ -3,12 +3,14 @@ package variance
 import "math-skills/average"
 
 func Variance(numbers []int) float64 {
-// variance is the the sum of the difference between each data point and the mean divide by the number of data points
+	// Calculate the mean of the input numbers
 	mean := average.Average(numbers)
 	var sum float64 = 0
+	// Calculate the sum of squares of the differences between each number and the mean
 	for _, n := range numbers {
 		sum += ((float64(n) - mean) * (float64(n) - mean)) // sum of squares
 	}
+	// Calculate the variance by dividing the sum of squares by the number of data points
 	v := sum / float64(len(numbers))
 	return v
 }
