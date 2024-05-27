efbb37f3fda2ecf92770bb12b1461c4289fe3bc2
wyonyango <wycliffealphus7@gmail.com>
2024-05-27 12:29:58 +0300

    style: add comments

--- standard/standard.go
+++ standard/standard.go
@@ -2,12 +2,17 @@ package standard
 
 import (
 	"math"
+
 	"math-skills/variance"
 )
 
 func Deviation(numb []int) float64 {
-
+	// Calculate the variance of the input numbers; call the variance function with the numb slice as the argument
 	v := variance.Variance(numb)
+
+	// Calculate the standard deviation by taking the square root of the variance
 	standard := math.Sqrt(v)
+
+	// Return the standard deviation
 	return standard
 }
