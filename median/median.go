f7e2e5c425b09ba8ec72b4c68addbfe263e1e688
wyonyango <wycliffealphus7@gmail.com>
2024-05-27 12:59:20 +0300

    style: add comments

--- median/median.go
+++ median/median.go
@@ -3,11 +3,11 @@ package median
 func Median(numbers []int) float64 {
 	var median float64
 
-	currIndex := len(numbers) / 2
-	if len(numbers)%2 == 0 { // if the number of available data points is even
-		median = (float64(numbers[currIndex]) + float64(numbers[currIndex-1])) / 2
+	currIndex := len(numbers) / 2 // Calculate the index for the middle element
+	if len(numbers)%2 == 0 {      // Check if the number of data points is even
+		median = (float64(numbers[currIndex]) + float64(numbers[currIndex-1])) / 2 // Calculate the median for even data points
 	} else if len(numbers)%2 != 0 {
-		median = (float64(numbers[currIndex])) // if the number of available data points is odd
+		median = (float64(numbers[currIndex])) // Calculate the median for odd data points
 	}
 	return median
 }
