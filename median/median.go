3e0bf47c5e384c87e482df78750262500f6f346f
wyonyango <wycliffealphus7@gmail.com>
2024-05-27 14:27:56 +0300

    feat: add sorting feature, in ascending order

--- median/median.go
+++ median/median.go
@@ -3,6 +3,14 @@ package median
 func Median(numbers []int) float64 {
 	var median float64
 
+	for j := 0; j < len(numbers); j++ { // Loop through the numbers slice
+		for i := 0; i < len(numbers); i++ { // Nested loop to compare adjacent elements
+			if (i+1) < len(numbers) && numbers[i+1] < numbers[i] { // Check if the next element is smaller
+				numbers[i], numbers[i+1] = numbers[i+1], numbers[i] // Swap the elements if needed
+			}
+		}
+	}
+
 	currIndex := len(numbers) / 2 // Calculate the index for the middle element
 	if len(numbers)%2 == 0 {      // Check if the number of data points is even
 		median = (float64(numbers[currIndex]) + float64(numbers[currIndex-1])) / 2 // Calculate the median for even data points
