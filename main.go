833e4f3e7f7eff8ecab2dbba8a1aca7fb521ee1b
wyonyango <wycliffealphus7@gmail.com>
2024-05-27 11:03:33 +0300

    style: Add commnents explaining error handling for strconv.Atoi

--- main.go
+++ main.go
@@ -34,6 +34,7 @@ func main() {
 					continue
 				}
 				numb, err := strconv.Atoi(split[i])
+				// Checking the type of error encountered by the Atoi function and tailoring the message according to the specidied message
 				if err != nil {
 					Error, ok := err.(*strconv.NumError)
 					if ok {
